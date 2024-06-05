96cda1a2c486ca529ee1cffb914ac8f3ad3643f0
Mateusz <matimusial5@gmail.com>
2024-06-05 10:55:02 +0200

    alert

--- CONTROLLERS/DoctorViewController.py
+++ CONTROLLERS/DoctorViewController.py
@@ -236,7 +236,7 @@ class DoctorViewController:
             self.db_conn.establish_connection()
 
         if self.db_conn.connection is None:
-            self.show_alert("Cannot establish database connection")
+            self.show_alert("Cannot establish database connection, remember to enable ZUT VPN.")
             return
 
         modelEEG = self.ui.modelListViewEEG.model()
