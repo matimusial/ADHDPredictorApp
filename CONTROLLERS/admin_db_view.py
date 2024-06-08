495062e5b8eaca82ad6e219587dcc2de21db5da9
Jacek Lal <61835186+Rudy5525@users.noreply.github.com>
2024-06-08 14:21:22 +0200

    resizing db view

--- CONTROLLERS/admin_db_view.py
+++ CONTROLLERS/admin_db_view.py
@@ -1,5 +1,5 @@
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QTableWidgetItem, QMessageBox
+from PyQt5.QtWidgets import QTableWidgetItem, QMessageBox, QHeaderView
 from PyQt5.QtCore import Qt
 
 from CONTROLLERS.DBConnector import DBConnector
@@ -52,7 +52,24 @@ class AdminDbView:
                 item.setFlags(Qt.ItemIsEnabled)
                 self.ui.tableWidget.setItem(row_num, col_num, item)
 
+        # Resize columns and rows to fit the contents
         self.ui.tableWidget.resizeColumnsToContents()
+        self.ui.tableWidget.resizeRowsToContents()
+
+        # Make columns stretch to fit the available space
+        header = self.ui.tableWidget.horizontalHeader()
+        header.setSectionResizeMode(QHeaderView.Stretch)
+
+        # Optional: Make rows stretch to fit the available space
+        # This can be adjusted if you want a different resizing behavior
+        vertical_header = self.ui.tableWidget.verticalHeader()
+        vertical_header.setSectionResizeMode(QHeaderView.ResizeToContents)
+
+        # Make the table widget resize with the window
+        self.ui.tableWidget.setSizeAdjustPolicy(QtWidgets.QAbstractScrollArea.AdjustToContents)
+
+        # Optionally, adjust the size policy to ensure proper resizing
+        self.ui.tableWidget.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
 
     def delete_row(self, id_table):
         """
